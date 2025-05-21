cf690e04f12545d97118ff4e00007045a73ca1dd
Kyle Rodgers <65290193+misterrodg@users.noreply.github.com>
2025-05-21 11:45:00 -0400

    Add Heli Waypoint Objects

---
+++ docs/Heli_Waypoint.md
@@ -0,0 +1,26 @@
+# The Heli Waypoint Object
+
+NOTE: The Heli Waypoint object is used in for both Terminal and Enroute waypoints.
+
+The Heli Waypoint object comprises the following fields:
+
+- st: A standard (S) versus tailored (T) identifier.
+- area: The world region (e.g. `USA` or `CAN`).
+- sec_code: The section code, used in parsing.
+- heliport: The associated heliport.
+- heliport_region: The region of the heliport.
+- heliport_sub_code: The sub code of the heliport.
+- waypoint_id: The ICAO code of the waypoint.
+- waypoint_region: The region of the waypoint.
+- cont_rec_no: An identifier for additional available data.
+- type: The waypoint type
+- usage: The use type (e.g. high, low, or both).
+- lat: The waypoint latitude.
+- lon: The waypoint longitude.
+- mag_var: The magnetic variation.
+- elevation: The elevation of the waypoint.
+- datum_code: The reference system used in surveying.
+- name_indicator: The name indicator of the waypoint.
+- name_description: The description of the name.
+- record_number: The CIFP record number.
+- cycle_data: The cycle ID of when the record was added/updated.
---
+++ src/cifparse/records/heli_waypoint/base.py
@@ -0,0 +1,95 @@
+from cifparse.functions.record import clean_value, convert_record_number, extract_field
+from cifparse.records.table_base import TableBase
+
+from .widths import w_bas
+
+
+class Base(TableBase):
+    st: str
+    area: str
+    sec_code: str
+    heliport: str
+    heliport_region: str
+    heliport_sub_code: str
+    waypoint_id: str
+    waypoint_region: str
+    record_number: int
+    cycle_data: str
+
+    def __init__(self, table_name: str):
+        super().__init__(table_name)
+        self.st = None
+        self.area = None
+        self.sec_code = None
+        self.heliport = None
+        self.heliport_region = None
+        self.heliport_sub_code = None
+        self.waypoint_id = None
+        self.waypoint_region = None
+        self.record_number = None
+        self.cycle_data = None
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}: {self.waypoint_id}"
+
+    def from_line(self, line: str) -> "Base":
+        self.st = extract_field(line, w_bas.st)
+        self.area = extract_field(line, w_bas.area)
+        self.sec_code = extract_field(line, w_bas.sec_code)
+        self.heliport = extract_field(line, w_bas.heliport)
+        self.heliport_region = extract_field(line, w_bas.heliport_region)
+        self.heliport_sub_code = extract_field(line, w_bas.heliport_sub_code)
+        self.waypoint_id = extract_field(line, w_bas.waypoint_id)
+        self.waypoint_region = extract_field(line, w_bas.waypoint_region)
+        self.record_number = convert_record_number(
+            extract_field(line, w_bas.record_number)
+        )
+        self.cycle_data = extract_field(line, w_bas.cycle_data)
+        return self
+
+    def ordered_leading(self) -> list:
+        return [
+            "st",
+            "area",
+            "sec_code",
+            "heliport",
+            "heliport_region",
+            "heliport_sub_code",
+            "waypoint_id",
+            "waypoint_region",
+        ]
+
+    def ordered_trailing(self) -> list:
+        return [
+            "record_number",
+            "cycle_data",
+        ]
+
+    def ordered_fields(self) -> dict:
+        result = []
+        result.extend(self.ordered_leading())
+        result.extend(self.ordered_trailing())
+        return result
+
+    def get_leading_dict(self) -> dict:
+        return {
+            "st": clean_value(self.st),
+            "area": clean_value(self.area),
+            "sec_code": clean_value(self.sec_code),
+            "heliport": clean_value(self.heliport),
+            "heliport_region": clean_value(self.heliport_region),
+            "heliport_sub_code": clean_value(self.heliport_sub_code),
+            "waypoint_id": clean_value(self.waypoint_id),
+            "waypoint_region": clean_value(self.waypoint_region),
+        }
+
+    def get_trailing_dict(self) -> dict:
+        return {
+            "record_number": clean_value(self.record_number),
+            "cycle_data": clean_value(self.cycle_data),
+        }
+
+    def to_dict(self):
+        leading = self.get_leading_dict()
+        trailing = self.get_trailing_dict()
+        return {**leading, **trailing}
